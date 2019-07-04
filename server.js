5eeb3f31bdd8f6c18fc9c89ffc822e6a765fb4c8
soapsabee <s5930213055@phuket.psu.ac.th>
2019-07-04 17:48:05 +0700

    'test-database'

--- server.js
+++ server.js
@@ -1,19 +1,36 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongo = require('mongodb');
-
+var mongoose = require('mongoose');
 const app = express();
 
 app.use(bodyParser.json());
 
-let collection = null;
+const url = process.env.MONGO_URI ;
+mongoose.connect(url);
 
- (async() => {
-    const url = process.env.MONGO_URI ;
-    const connection = await mongo.connect(url);
-    const db = connection.db('sisconned-db')
-    collection = db.collection('student');
-})
+var db = mongoose.connection;
+
+db.on('connected', function(){
+
+    console.log('Mongoose connected');
+    
+    });
+    
+    // เมื่อการเชื่อมต่อไม่สำเร็จ
+    
+    db.on('error', function(err){
+    
+    console.log('Mongoose error: ' + err);
+    
+    });
+
+
+//  (async() => {
+//     const url = process.env.MONGO_URI ;
+//     const connection = await mongo.connect(url);
+//     const db = connection.db('sisconned-db')
+//     collection = db.collection('student');
+// })
 
 app.get('/', (req,res)=> {
     res.send('Welcome1');
